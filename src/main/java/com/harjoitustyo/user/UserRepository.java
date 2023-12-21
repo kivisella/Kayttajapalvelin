93422e337822285273d4392413a81beb6a22f9f1
kivisella <102664461+kivisella@users.noreply.github.com>
2023-12-21 17:37:44 +0200

    Harjoitustyö
    Tässä palvelinohjelmoinnin harjoitustyö

---
+++ src/main/java/com/harjoitustyo/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.harjoitustyo.user;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    public Long countById(Integer id);
+}
